4eeef97a031943b065ccdf162e8ea10a848537f9
Julien <parsy.julien@hotmail.fr>
2023-09-25 16:30:24 +0200

    Amélioration de la page d'accueil

--- app.js
+++ app.js
@@ -2,8 +2,23 @@ const express = require('express')
 const app = express()
 const port = process.env.PORT || 3000;
 
+
 app.get('/', (req, res) => {
-  res.send('Hello World!')
+  res.send(`
+    < !DOCTYPE html >
+  <html lang="en">
+    <head>
+      <meta charset="UTF-8" />
+      <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      <title>Document</title>
+    </head>
+    <body>
+
+    </body>
+  </html>
+    
+ res.send(html)
+ `)
 })
 
 app.listen(port, () => {

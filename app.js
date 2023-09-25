a63980916d85f58a5c4984dedddff220d914ad53
Julien <parsy.julien@hotmail.fr>
2023-09-25 16:39:21 +0200

    Fixed

--- app.js
+++ app.js
@@ -5,20 +5,20 @@ const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   res.send(`
-    < !DOCTYPE html >
-  <html lang="en">
+    <!DOCTYPE html >
+    <html lang="en">
     <head>
       <meta charset="UTF-8" />
       <meta name="viewport" content="width=device-width, initial-scale=1.0" />
       <title>Document</title>
     </head>
     <body>
-
+      <h1>Hello World</h1>
     </body>
   </html>
     
- res.send(html)
- `)
+  `)
+  res.send(html)
 })
 
 app.listen(port, () => {
